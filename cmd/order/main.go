e6d778932816dd1f7457c6bdc058b69a98eac8ec
Gustavo Henrique <gustavo._henrique@hotmail.com>
2023-12-31 10:02:42 -0300

    feat: added rabbitmq

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 
 	"github.com/gustavohenriquess/go-intensive23/internal/Infra/database"
 	usecase "github.com/gustavohenriquess/go-intensive23/internal/useCase"
+	"github.com/gustavohenriquess/go-intensive23/pkg/rabbitmq"
 	_ "github.com/mattn/go-sqlite3"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -14,21 +17,32 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-
+	defer db.Close() // espera tudo rodar e depois executa o close
 	orderRepository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPrice(orderRepository)
-
-	input := usecase.OrderInput{
-		ID:    "2",
-		Price: 10.0,
-		Tax:   1.0,
-	}
-
-	output, err := uc.Execute(input)
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
 	}
+	defer ch.Close()
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgRabbitmqChannel) // escutando a fila // trava // T2
+	rabbitmqWorker(msgRabbitmqChannel, uc)      // T1
+}
 
-	fmt.Println(output)
+func rabbitmqWorker(msgChan chan amqp.Delivery, uc *usecase.CalculateFinalPrice) {
+	fmt.Println("Starting rabbitmq")
+	for msg := range msgChan {
+		var input usecase.OrderInput
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			panic(err)
+		}
+		output, err := uc.Execute(input)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Println("Mensagem processada e salva no banco:", output)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require (
 	github.com/mattn/go-sqlite3 v1.14.18
+	github.com/rabbitmq/amqp091-go v1.9.0
+	github.com/streadway/amqp v1.1.0
 	github.com/stretchr/testify v1.8.4
 )
 
