247fb5c0195164e9682965a56c6e7b498b469466
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-01-07 11:37:03 -0300

    feat: api improvement, changed GET to POST and now saving in SQLITE3

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"io"
 	"net/http"
 
-	"github.com/gustavohenriquess/go-intensive23/internal/entity"
+	"github.com/gustavohenriquess/go-intensive23/internal/Infra/database"
+	usecase "github.com/gustavohenriquess/go-intensive23/internal/useCase"
 	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type OrderRequest struct {
+	Price float64 `json:"price"`
+	Tax   float64 `json:"tax"`
+}
 
 func main() {
 	//Chi
@@ -14,17 +24,40 @@ func main() {
 	// r.Get("/order", OrderHandler)
 	// http.ListenAndServe(":8888", r)
 	e := echo.New()
-	e.GET("/order", OrderHandler)
+	e.POST("/order", OrderHandler)
+
 	e.Logger.Fatal(e.Start(":8888"))
+
 }
 
 func OrderHandler(c echo.Context) error {
-	order, _ := entity.NewOrder(10.0, 0.1)
-	err := order.CalculateFinalPrice()
+	db, err := sql.Open("sqlite3", "db.sqlite3")
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err)
 	}
-	return c.JSON(http.StatusOK, order)
+	defer db.Close() // espera tudo rodar e depois executa o close
+
+	orderRepository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
+
+	body, err := io.ReadAll(c.Request().Body)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	// Decodifica o JSON
+	var bodyJson usecase.OrderInput
+	err = json.Unmarshal(body, &bodyJson)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	output, err := uc.Execute(bodyJson)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, output)
 }
 
 // func OrderHandler(w http.ResponseWriter, r *http.Request) {
